c4d3d1c1bddff84f74b5bd0b7ffb227a3ef87477
shubh9752 <shubhamkumar3.sk@gmail.com>
2023-08-31 09:22:57 -0700

    simple todo created

---
+++ todoList/script.js
@@ -0,0 +1,35 @@
+const form=document.querySelector("form");
+const input=document.querySelector("input");
+const list=document.querySelector("ul");
+
+
+form.addEventListener("submit",function (e){
+    e.preventDefault();
+    todo();
+    input.value="";
+
+
+})
+function todo(){
+    if(input.value===""){
+        alert("please type something");
+    }else{
+        const value=input.value;
+        const newli=document.createElement("li");
+        newli.textContent=value;
+        const deletebtn=document.createElement("button");
+        deletebtn.textContent="delete";
+       
+
+        list.append(newli);
+        newli.append(" ",deletebtn);
+        deletebtn.addEventListener("click",function(){
+
+        })
+    }
+}
+list.addEventListener("click",function (e){
+    if(e.target.nodeName==="BUTTON"){
+        e.target.closest("LI").remove();
+    }
+})
